e18d9211bf48eaa803b6447e0b698d95489d34d2
Thomas <thomasbulle@hotmail.fr>
2020-02-08 01:53:48 +0100

    Add about section

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import 'App.scss';
 
 // Components
 import Header from 'components/Header/Header';
+import About from 'components/About/About';
 
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <About />
     </div>
   );
 }
---
+++ src/components/About/About.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './styles.scss';
+import { FormattedHTMLMessage, FormattedMessage } from 'react-intl';
+
+class About extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className="about-wrapper">
+        <h2><FormattedMessage id="about.title" /></h2>
+
+        <img src="ressources/images/profile-picture.jpg" alt="Profile" className="profile-picture" />
+
+        <div className="description-container">
+          <FormattedHTMLMessage id="about.description" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default About;
